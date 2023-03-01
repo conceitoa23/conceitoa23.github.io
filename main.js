5e4843e761d27ddbf17427713b45a91d80a39dbe
conceitoa23 <126684172+conceitoa23@users.noreply.github.com>
2023-03-01 17:29:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -55,14 +55,14 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 const mtlloader = new MTLLoader();
-mtlloader.setPath('https://github.com/conceitoa23/conceitoa23.github.io');
+mtlloader.setPath('../');
 mtlloader.load(
     'caixinhapendrive.mtl', (materials) => {
     materials.preload();
 
     const objloader = new OBJLoader();
     objloader.setMaterials(materials);
-    objloader.setPath('https://github.com/conceitoa23/conceitoa23.github.io');
+    objloader.setPath('../');
     objloader.load(
         'caixinhapendrive.obj', 
         (object) => {
